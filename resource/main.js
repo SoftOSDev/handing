37adcf6255919e6194afe03a16e19fd4a92412f2
SoftOSDev <117074497+SoftOSDev@users.noreply.github.com>
2023-01-01 16:24:29 +0800

    Add files via upload

---
+++ resource/main.js
@@ -0,0 +1,45 @@
+function HandingOS(){
+	window.location.replace("https://pan.baidu.com/s/1ZW8zfFVCd0ZIRkOI7AJRGQ?pwd=haos");
+}
+function XihaoOS(){
+	window.location.replace("https://cloud.189.cn/web/share?code=6rYVZzjeeMrm#访问码是um6f");
+}
+function MacDock(){
+	window.location.replace("https://codeload.github.com/SoftOSDev/MacDock_Download/zip/refs/heads/main");
+}
+function QLZ(){
+	window.location.replace("https://codeload.github.com/SoftOSDev/WindowsLoading-Download/zip/refs/heads/main");
+}
+function ZXSQ(){
+	window.location.replace("https://codeload.github.com/SoftOSDev/ZXSQ/zip/refs/heads/main");
+}
+function JTTL(){
+	window.location.replace("https://codeload.github.com/SoftOSDev/JTTL/zip/refs/heads/main");
+}
+/*function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"; "+expires;
+}
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
+}
+function checkCookie(){
+	var user=getCookie("username");
+	if (user!=""){
+		
+	}
+	else {
+		user = prompt("请输入你的名字:","");
+  		if (user!="" && user!=null){
+    		setCookie("username",user,30);
+    	}
+	}
+}*/
